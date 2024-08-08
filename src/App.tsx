960f9530dbae45c402732498c5722f0a92a299cf
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-08 20:08:19 -0300

    Completando o projeto com a estilização do home, inclusão do axios, tela de cadastro e login

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './App.css';
-import Home from './paginas/home/Home';
+
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from '../src/paginas/Login/Login';
+import Cadastro from '../src/paginas/cadastro/Cadastro';
+import Home from '../src/paginas/home/Home';
+import { AuthProvider } from '../src/paginas/contexts/AuthContext';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;

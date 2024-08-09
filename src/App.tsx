2dacdc6c43e227e199729924abcf3c9edda49cbd
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-09 17:01:15 -0300

    Implementando novas funções de tema

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Login from '../src/paginas/Login/Login';
 import Cadastro from '../src/paginas/cadastro/Cadastro';
 import Home from '../src/paginas/home/Home';
 import { AuthProvider } from '../src/paginas/contexts/AuthContext';
+import ListaTemas from './temas/listaTemas/ListaTemas';
+import FormularioTema from '../src/temas/formularioTema/FormularioTema'
+import DeletarTema from './temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -22,6 +25,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />

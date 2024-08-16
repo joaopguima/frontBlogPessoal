fc904aedd25d7a73bc0c69395d03bc9130a3d9cb
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-16 20:29:24 -0300

    Ajustando meu blog

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,44 +3,37 @@ import { InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Footer() {
+  const { usuario } = useContext(AuthContext);
 
-  const { usuario } = useContext(AuthContext)
+  let footerComponent;
 
-  let footerComponent
-  
-
-  if (usuario.token !== '') {
-    footerComponent = ( 
+  if (usuario.token !== "") {
+    footerComponent = (
       <>
-      <div className="flex justify-center bg-indigo-900 text-white py-4">
-        <div className="container flex flex-col items-center py-4">
-          <p className="text-xl font-bold">
-            Blog pessoal João Pedro | Copyright:{" "}
-          </p>
-          <p className="text-lg">Acesse minhas redes sociais</p>
-          <div className="flex gap-2">
-            <a href="https://www.linkedin.com/in/joaopguima/" target="blank">
-              <LinkedinLogo
-                alt="Linkedin do João Pedro"
-                size={48}
-                weight="bold"
-              />
-            </a>
-            <a href="https://www.instagram.com/joaopguima__/" target="blank">
-              <InstagramLogo size={48} weight="bold" />
-            </a>
+        <div className="flex justify-center bg-indigo-900 text-white py-4">
+          <div className="container flex flex-col items-center py-4">
+            <p className="text-xl font-bold">
+              Blog pessoal João Pedro | Copyright:{" "}</p>
+            <p className="text-lg">Acesse minhas redes sociais</p>
+            <div className="flex gap-2">
+              <a href="https://www.linkedin.com/in/joaopguima/" target="blank">
+                <LinkedinLogo
+                  alt="Linkedin do João Pedro"
+                  size={48}
+                  weight="bold"
+                />
+              </a>
+              <a href="https://www.instagram.com/joaopguima__/" target="blank">
+                <InstagramLogo size={48} weight="bold" />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  )   
-}
+      </>
+    );
+  }
 
-  return (
-    <>
-    {footerComponent}
-    </>
-  )
+  return <>{footerComponent}</>;
 }
 
 export default Footer;
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -35,7 +35,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
         setIsLoading(true)
         try {
             await login(`/usuarios/logar`, userLogin, setUsuario)
-            toastAlerta('Você precisa estar logado', 'info');
+            toastAlerta('Usuário logado com sucesso', 'sucesso');
             setIsLoading(false)
 
         } catch (error) {
